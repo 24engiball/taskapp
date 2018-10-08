47b6d7ef5935675274a75b0e02f6b10009843e95
Hades <hades@Hadess-MacBook-Pro.local>
2018-10-08 15:22:42 +0700

    commit

--- backend/build_image.sh
+++ backend/build_image.sh
@@ -2,5 +2,5 @@
 docker rmi taskapi:0.0.3
 docker container run --rm -v $(pwd):/src -w /src composer update
 docker image build -t taskapi .
-docker image tag taskapi:latest engiball/taskapi:0.0.2
+docker image tag taskapi:latest engiball/taskapi:0.0.3
 docker push engiball/taskapi:0.0.3
